7ec5082412294b5e190005286a294de5686a6ead
kikislol <133943251+kikislol@users.noreply.github.com>
2023-06-30 20:51:38 +0700

    create routes and nav bar WIP

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
+import About from "./about/About.js";
+import Skills from "./skills/Skills.js";
+import Projects from "./projects/Projects.js";
+import Contact from "./contact/Contact.js";
+
+const App = () => {
+    return (
+        <Router>
+            <Nav />
+            <Routes>
+                <Route path="/" element={About} />
+                <Route path="/skills" skills element={Skills} />
+                <Route path="/projects" element={Projects} />
+                <Route path="/contact" element={Contact} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
